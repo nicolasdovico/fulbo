183ecfea814cc89f262bb10833c6b3017df6a235
nicolasdovico <nicolasdovico@gmail.com>
2022-04-19 21:44:53 -0300

    es un commit de prueba, pero con datos posta

--- metadatos/permisos/grupo_acceso__admin.sql
+++ metadatos/permisos/grupo_acceso__admin.sql
@@ -29,3 +29,18 @@ INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, i
 	'2'  --item
 );
 --- FIN Grupo de desarrollo 0
+
+--- INICIO Grupo de desarrollo 10
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'fulbo', --proyecto
+	'admin', --usuario_grupo_acc
+	NULL, --item_id
+	'10000026'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'fulbo', --proyecto
+	'admin', --usuario_grupo_acc
+	NULL, --item_id
+	'10000027'  --item
+);
+--- FIN Grupo de desarrollo 10
