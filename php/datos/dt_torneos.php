9aa22292e88d07c4e4cd31a723cc98da6e30b8f6
nicolasdovico <nicolasdovico@gmail.com>
2022-06-12 22:32:57 -0300

    envio el abm de torneos

--- php/datos/dt_torneos.php
+++ php/datos/dt_torneos.php
@@ -1,23 +1,27 @@
 <?php
+
+
 class dt_torneos extends toba_datos_tabla
 {
 	function get_listado($filtro=array())
 	{
 		$where = array();
 		if(isset($filtro['tor_desc'])) {
-			$where[] = "tor_desc ILIKE ".quote("%{$filtro['tor_desc']}%");
+			$where[] = "tor_desc ILIKE ".quote("%{$filtro['tor_desc']['valor']}%");
 		}
 		$sql = "SELECT
-	t.tor_desc,
-	t.tor_id
-FROM
-	torneos as t
-ORDER BY tor_desc";
+			t.tor_desc,
+			t.tor_id
+		FROM
+			torneos as t
+		ORDER BY tor_desc";
 		if(count($where)>0) {
 			$sql = sql_concatenar_where($sql, $where);
 		}
 		return toba::db('fulbo')->consultar($sql);
 	}
+
+	
 }
 
 ?>
