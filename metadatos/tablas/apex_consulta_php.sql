1eef4c739de3599940284664994972c7463d4d4d
nicolasdovico <nicolasdovico@gmail.com>
2022-06-17 10:22:53 -0300

    envio la parte de cargar partidos

---
+++ metadatos/tablas/apex_consulta_php.sql
@@ -0,0 +1,16 @@
+
+------------------------------------------------------------
+-- apex_consulta_php
+------------------------------------------------------------
+
+--- INICIO Grupo de desarrollo 10
+INSERT INTO apex_consulta_php (proyecto, consulta_php, clase, archivo_clase, archivo, descripcion, punto_montaje) VALUES (
+	'fulbo', --proyecto
+	'10000001', --consulta_php
+	'consultas_sql', --clase
+	'consultas_sql', --archivo_clase
+	'lib/consultas_sql.php', --archivo
+	NULL, --descripcion
+	'10000001'  --punto_montaje
+);
+--- FIN Grupo de desarrollo 10
